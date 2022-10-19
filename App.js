ebfdb65351190f59c77b52184431553ede987849
Christina C <jk5955762@gmail.com>
2022-10-19 16:38:26 -0400

    Initial code for sign up page

--- App.js
+++ App.js
@@ -1,11 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Alert,Text, View, StyleSheet, Button, TextInput } from 'react-native';
+import Constants from 'expo-constants';
 
 export default function App() {
+  const [userName, onChangeUserName] = React.useState("");
+  const [email, onChangeEmail] = React.useState("");
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.header}>
+        Start your foodie journey here
+      </Text>
+      <TextInput 
+      style={styles.input}
+      value={userName}
+      onChangeText={onChangeUserName} //prop of TextInput
+      placeholder = "User name"
+      />
+
+      <TextInput 
+      style={styles.input}
+      value={email}
+      onChangeText={onChangeEmail}
+      placeholder = "Email"
+      />
+      <Button
+        title="Sign up!"
+        onPress={() => Alert.alert(`Hello ${userName} (${email})`)}
+      />
     </View>
   );
 }
@@ -13,8 +35,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#D5ECFD',
+    padding: 8,
   },
+  header: {
+    fontSize: 36,
+    fontWeight: 'bold',
+    textAlign: 'center'
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  }
 });
